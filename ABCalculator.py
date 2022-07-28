47809be8b39927f50f3d317be31a0d306e6be664
asenachin <senachin.ap@yandex.ru>
2022-07-28 11:32:52 +0700

    Считывание данных из полей ввода

--- ABCalculator.py
+++ ABCalculator.py
@@ -5,8 +5,18 @@ import tkinter as tk
 # Функция закрытия программы
 def do_close():
     root.destroy()
-
-def popup_window():
+    
+def do_processing():
+    # Считываем данные из полей ввода
+    n1 = int(entVisitors1.get())
+    c1 = int(entConversions1.get())
+    n2 = int(entVisitors2.get())
+    c2 = int(entConversions2.get())
+    
+    popup_window(n1, c1, n2, c2)
+    
+# Функция вызова окна результатов
+def popup_window(n1, c1, n2, c2):
     window = tk.Toplevel()
     window.geometry("300x300")
     window.title("А/В калькулятор")
@@ -14,6 +24,9 @@ def popup_window():
     # Добавление кнопки закрытия окна
     btnClosePopup = tk.Button(window, text="Закрыть", font=('Helvetica', 10, 'bold'), command=window.destroy)
     btnClosePopup.place(x=180, y=250, width=90, height=30)
+    
+    # Перевод фокуса на созданное окно
+    window.focus_force()
 
 # Создание главного окна
 root = tk.Tk()
@@ -32,14 +45,16 @@ lblTitle1.place(x=25, y=55)
 lblVisitors1 = tk.Label(text="Посетители:", font=('Helvetica', 10, 'bold'))
 lblVisitors1.place(x=25, y=85)
 
-lblVisitors1 = tk.Entry(font=('Helvetica', 10, 'bold'))
-lblVisitors1.place(x=115, y=85, width=90, height=20)
+entVisitors1 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
+entVisitors1.place(x=115, y=85, width=90, height=20)
+entVisitors1.insert(tk.END, '0')
 
 lblConversions1 = tk.Label(text="Конверсии:", font=('Helvetica', 10, 'bold'))
 lblConversions1.place(x=25, y=115)
 
-lblConversions1 = tk.Entry(font=('Helvetica', 10, 'bold'))
-lblConversions1.place(x=115, y=115, width=90, height=20) 
+entConversions1 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
+entConversions1.place(x=115, y=115, width=90, height=20) 
+entConversions1.insert(tk.END, '0')
 
 # Добавление метки заголовка тестовой группы
 lblTitle2 = tk.Label(text="Посетители", font=('Helvetica', 12, 'bold'), fg='#008800')
@@ -49,17 +64,19 @@ lblTitle2.place(x=25, y=145)
 lblVisitors2 = tk.Label(text="Посетители:", font=('Helvetica', 10, 'bold'))
 lblVisitors2.place(x=25, y=175)
 
-lblVisitors2 = tk.Entry(font=('Helvetica', 10, 'bold'))
-lblVisitors2.place(x=115, y=175, width=90, height=20)
+entVisitors2 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
+entVisitors2.place(x=115, y=175, width=90, height=20)
+entVisitors2.insert(tk.END, '0')
 
 lblConversions2 = tk.Label(text="Конверсии:", font=('Helvetica', 10, 'bold'))
 lblConversions2.place(x=25, y=205)
 
-lblConversions2 = tk.Entry(font=('Helvetica', 10, 'bold'))
-lblConversions2.place(x=115, y=205, width=90, height=20)
+entConversions2 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
+entConversions2.place(x=115, y=205, width=90, height=20)
+entConversions2.insert(tk.END, '0')
 
 # Добавление кнопки "Расчитать"
-btnProcess = tk.Button(root, text="Расчитать", font=('Helvetica', 10, 'bold'), command=popup_window)
+btnProcess = tk.Button(root, text="Расчитать", font=('Helvetica', 10, 'bold'), command=do_processing)
 btnProcess.place(x=25, y=250, width=90, height=30)
 
 # Добавление кнопки закрытия программы
